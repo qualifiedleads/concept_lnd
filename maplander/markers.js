4a46f6ebf5452b7687025c59b2b4f00f4c8bcc79
Jen Brannstrom <jen.brannstrom@morequalifiedleads.co.uk>
2014-02-03 13:11:28 +0200

    "age" to Hebrew

--- maplander/markers.js
+++ maplander/markers.js
@@ -1,64 +1,64 @@
 var datass = {
     net: [{
             name: "קרן בוסקילה",
-            age: 26,
-            pin: "http://commondatastorage.googleapis.com/lnd/mob/1p.png",
-            avatar: "http://commondatastorage.googleapis.com/lnd/mob/1.jpg"
+            גיל: 26,
+            pin: "http://commondatastorגיל.googleapis.com/lnd/mob/1p.png",
+            avatar: "http://commondatastorגיל.googleapis.com/lnd/mob/1.jpg"
         }, {
             name: "ריבה",
-            age: 37,
-            pin: "http://commondatastorage.googleapis.com/lnd/mob/2p.png",
-            avatar: "http://commondatastorage.googleapis.com/lnd/mob/2.jpg"
+            גיל: 37,
+            pin: "http://commondatastorגיל.googleapis.com/lnd/mob/2p.png",
+            avatar: "http://commondatastorגיל.googleapis.com/lnd/mob/2.jpg"
         }, {
             name: "אושרת רומנו",
-            age: 31,
-            pin: "http://commondatastorage.googleapis.com/lnd/mob/3p.png",
-            avatar: "http://commondatastorage.googleapis.com/lnd/mob/3.jpg"
+            גיל: 31,
+            pin: "http://commondatastorגיל.googleapis.com/lnd/mob/3p.png",
+            avatar: "http://commondatastorגיל.googleapis.com/lnd/mob/3.jpg"
         }, {
             name: "Alana Muramats",
-            age: 24,
-            pin: "http://commondatastorage.googleapis.com/lnd/mob/4p.png",
-            avatar: "http://commondatastorage.googleapis.com/lnd/mob/4.jpg"
+            גיל: 24,
+            pin: "http://commondatastorגיל.googleapis.com/lnd/mob/4p.png",
+            avatar: "http://commondatastorגיל.googleapis.com/lnd/mob/4.jpg"
         }, {
             name: "Kayla Barclay",
-            age: 27,
-            pin: "http://commondatastorage.googleapis.com/lnd/mob/5p.png",
-            avatar: "http://commondatastorage.googleapis.com/lnd/mob/5.jpg"
+            גיל: 27,
+            pin: "http://commondatastorגיל.googleapis.com/lnd/mob/5p.png",
+            avatar: "http://commondatastorגיל.googleapis.com/lnd/mob/5.jpg"
         }, {
             name: "Caitlin Stawell",
-            age: 26,
-            pin: "http://commondatastorage.googleapis.com/lnd/mob/6p.png",
-            avatar: "http://commondatastorage.googleapis.com/lnd/mob/6.jpg"
+            גיל: 26,
+            pin: "http://commondatastorגיל.googleapis.com/lnd/mob/6p.png",
+            avatar: "http://commondatastorגיל.googleapis.com/lnd/mob/6.jpg"
         }, {
             name: "Lauren Monson",
-            age: 25,
-            pin: "http://commondatastorage.googleapis.com/lnd/mob/7p.png",
-            avatar: "http://commondatastorage.googleapis.com/lnd/mob/7.jpg"
+            גיל: 25,
+            pin: "http://commondatastorגיל.googleapis.com/lnd/mob/7p.png",
+            avatar: "http://commondatastorגיל.googleapis.com/lnd/mob/7.jpg"
         }, {
             name: "Stella Doolette",
-            age: 22,
-            pin: "http://commondatastorage.googleapis.com/lnd/mob/8p.png",
-            avatar: "http://commondatastorage.googleapis.com/lnd/mob/8.jpg"
+            גיל: 22,
+            pin: "http://commondatastorגיל.googleapis.com/lnd/mob/8p.png",
+            avatar: "http://commondatastorגיל.googleapis.com/lnd/mob/8.jpg"
         }, {
             name: "Sophie Lindell",
-            age: 21,
-            pin: "http://commondatastorage.googleapis.com/lnd/mob/9p.png",
-            avatar: "http://commondatastorage.googleapis.com/lnd/mob/9.jpg"
+            גיל: 21,
+            pin: "http://commondatastorגיל.googleapis.com/lnd/mob/9p.png",
+            avatar: "http://commondatastorגיל.googleapis.com/lnd/mob/9.jpg"
         }, {
             name: "Alicia Tout",
-            age: 25,
-            pin: "http://commondatastorage.googleapis.com/lnd/mob/10p.png",
-            avatar: "http://commondatastorage.googleapis.com/lnd/mob/10.jpg"
+            גיל: 25,
+            pin: "http://commondatastorגיל.googleapis.com/lnd/mob/10p.png",
+            avatar: "http://commondatastorגיל.googleapis.com/lnd/mob/10.jpg"
         }, {
             name: "Stephanie Knorr",
-            age: 26,
-            pin: "http://commondatastorage.googleapis.com/lnd/mob/11p.png",
-            avatar: "http://commondatastorage.googleapis.com/lnd/mob/11.jpg"
+            גיל: 26,
+            pin: "http://commondatastorגיל.googleapis.com/lnd/mob/11p.png",
+            avatar: "http://commondatastorגיל.googleapis.com/lnd/mob/11.jpg"
         }, {
             name: "Lauren Flierl",
-            age: 24,
-            pin: "http://commondatastorage.googleapis.com/lnd/mob/12p.png",
-            avatar: "http://commondatastorage.googleapis.com/lnd/mob/12.jpg"
+            גיל: 24,
+            pin: "http://commondatastorגיל.googleapis.com/lnd/mob/12p.png",
+            avatar: "http://commondatastorגיל.googleapis.com/lnd/mob/12.jpg"
         }
     ]
 };
