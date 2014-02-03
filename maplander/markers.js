3fddced835ba26bbec6e92a2a8c7b1b491ddb31d
Jen Brannstrom <jen.brannstrom@morequalifiedleads.co.uk>
2014-02-03 13:25:51 +0200

    name edits

--- maplander/markers.js
+++ maplander/markers.js
@@ -1,51 +1,51 @@
 var datass = {
     net: [{
-            name: "שירי_קדוש",
+            name: "שירי קדוש",
             age: 26,
             pin: "http://commondatastorage.googleapis.com/lnd/mob/1p.png",
             avatar: "http://commondatastorage.googleapis.com/lnd/mob/1.jpg"
         }, {
-            name: "שגיא_מילר",
+            name: "שגיא מילר",
             age: 37,
             pin: "http://commondatastorage.googleapis.com/lnd/mob/2p.png",
             avatar: "http://commondatastorage.googleapis.com/lnd/mob/2.jpg"
         }, {
-            name: "ענת_סמל",
+            name: "ענת סמל",
             age: 31,
             pin: "http://commondatastorage.googleapis.com/lnd/mob/3p.png",
             avatar: "http://commondatastorage.googleapis.com/lnd/mob/3.jpg"
         }, {
-            name: "אייל_ארצי",
+            name: "אייל ארצי",
             age: 24,
             pin: "http://commondatastorage.googleapis.com/lnd/mob/4p.png",
             avatar: "http://commondatastorage.googleapis.com/lnd/mob/4.jpg"
         }, {
-            name: "נורית_רז",
+            name: "נורית רז",
             age: 27,
             pin: "http://commondatastorage.googleapis.com/lnd/mob/5p.png",
             avatar: "http://commondatastorage.googleapis.com/lnd/mob/5.jpg"
         }, {
-            name: "מורן_נחמיאס",
+            name: "מורן נחמיאס",
             age: 26,
             pin: "http://commondatastorage.googleapis.com/lnd/mob/6p.png",
             avatar: "http://commondatastorage.googleapis.com/lnd/mob/6.jpg"
         }, {
-            name: "לימור_ירון",
+            name: "לימור ירון",
             age: 25,
             pin: "http://commondatastorage.googleapis.com/lnd/mob/7p.png",
             avatar: "http://commondatastorage.googleapis.com/lnd/mob/7.jpg"
         }, {
-            name: "גלינה_ראובני",
+            name: "גלינה ראובני",
             age: 22,
             pin: "http://commondatastorage.googleapis.com/lnd/mob/8p.png",
             avatar: "http://commondatastorage.googleapis.com/lnd/mob/8.jpg"
         }, {
-            name: "בן_ירושלמי",
+            name: "בן ירושלמי",
             age: 21,
             pin: "http://commondatastorage.googleapis.com/lnd/mob/9p.png",
             avatar: "http://commondatastorage.googleapis.com/lnd/mob/9.jpg"
         }, {
-            name: "אנה_טרבלסי",
+            name: "אנה טרבלסי",
             age: 25,
             pin: "http://commondatastorage.googleapis.com/lnd/mob/10p.png",
             avatar: "http://commondatastorage.googleapis.com/lnd/mob/10.jpg"
