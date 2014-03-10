e197ad0374b8572fab7a8ada2cca126105188803
Jen Brannstrom <jen.brannstrom@morequalifiedleads.co.uk>
2014-03-10 12:32:23 +0200

    Fonts to Arial

---
+++ mob/mehina-small.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html dir="rtl" lang="he"> 
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0, user-scalable=yes" />
+    <title>מילגה לשנקר או בצלאל</title>
+    <link rel="stylesheet" href="http://mob-css.commondatastorage.googleapis.com/reset.css" media="Screen" type="text/css" />
+    <link rel="stylesheet" href="http://mob-css.commondatastorage.googleapis.com/base.css" media="Screen" type="text/css" />
+    <link rel="stylesheet" href="http://mob-css.commondatastorage.googleapis.com/mobile.css" media="handheld, only screen and (max-width: 480px), only screen and (max-device-width: 480px)" type="text/css" />
+
+    <!--[if IEMobile]>
+    <link rel="stylesheet" href="mobile.css" media="screen" type="text/css"  />
+    <![endif]-->
+</head>
+  <body>
+        <!-- Google Tag Manager -->
+       <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-23J6"
+       height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+       <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+       '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+       })(window,document,'script','dataLayer','GTM-23J6');</script>
+       <!-- End Google Tag Manager -->
+
+    <div class="maincontent"><div class="row">
+    <div class="content">
+    	<div class="header-top">
+        <!-- Left Side-->
+        <section class="leftsidecol">
+        	<div class="ten columns">
+            <div class="leftside">
+            <!--Post-->
+            <div class="post">
+
+            <img src="http://lnd.commondatastorage.googleapis.com/scholarship300w.jpg" alt="image">
+            
+<h2>  המכינה המומלצת להכנת תיקי עבודות!<br> </h2>
+                <p>
+                <ul>
+                 <li>מילגה של 1000 ש' לפונים מעמוד זה!</li>
+                <li>אנו מתחייבים לקבלתכם לשנקר, בצלאל, HIT, אונ' ת"א, הטכניון, ויצ"ו חיפה או מכללה למנהל</li>
+                <li>המכינה המעשית ביותר, פחות תאוריות יותר התנסות חוויתית! </li>
+                </ul>
+<!--Contactform-->
+<div class="contactform">
+<?php if (isset($_GET['contactErr']) && (int)$_GET['contactErr'] === 1): ?>
+      <p style="color:red;font-weight:bold">Failed, please Call +972 3 561 2029</p>
+    <?php endif; ?>
+<span class="header"><h3>מעוניין ללמוד ב:</h3></span>
+    <form name="contact_form" method="post" action="http://www.concept-academy.co.il/lnd/mob/mobile.php">
+      <div class="form-row">
+      <fieldset>
+        <select name="uni" id="uni" class="icon-menu">
+            <option value='shenkar'>שנקר</option>
+            <option value='bezalel'>בצלאל</option>
+            <option value='HIT'>HIT</option>
+            <option value='technion'>טכניון</option>
+            <option value='TAV-university'>אונ' ת"א</option>
+        </select>
+      </div>
+      <div class="form-row">
+        <input type="text" name="name" placeholder="שם:"/>
+      </div>
+      <div class="form-row">
+        <input type="tel" name="phone" pattern="^[0-9]+[\- ]*[0-9]+$" placeholder="טלפון:"/>
+      </div>
+
+      <input type="submit" value="הגש בקשה"/>
+    </form>
+
+</div>
+<!--End Contactform-->
+                <ul>
+                <li>אצלנו משלמים רק פעם אחת עד שמתקבלים!<br>
+                </li>
+                <li>שיטה ייחודית ללימודי רישום, גם מי שלא יודע להחזיק עפרון, יהיה מופתע מהתוצאות!!!</li>
+                <li>מפגשים אישיים אחד על אחד להכנת תיק העבודות</li>
+                <li>אבחון והתאמה מקצועית אישית </li>
+                <li><strong>מוכר למענק חיילים משוחררים</strong></li>
+              </ul>
+              * כמות המילגות מוגבלת, הבטח את זכאותך
+              </p>
+
+        		</div>
+                
+                </div>
+                </div>
+                </div>
+                </div>
+                </div>
+        	</div>
+        </section>
+
+      </body>
+</html>
