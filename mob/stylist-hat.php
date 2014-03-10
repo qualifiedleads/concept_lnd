3af7eb082682685b7e8a639f3cd35d52fc7aa38d
Jen Brannstrom <jen.brannstrom@morequalifiedleads.co.uk>
2014-03-10 13:23:31 +0200

    change to PHP

---
+++ mob/stylist-hat.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="he" dir="rtl">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+</head>
+<title>קריירה בסטיילינג מתחילה פה</title>
+<style>
+h1{ font-size: 1.5rem; font-family: Arial, Helvetica, sans-serif;}
+h2{ font-size: 1.2rem; font-family: Arial, Helvetica, sans-serif;}
+html {font-size: 12pt; font-family: Arial, Helvetica, sans-serif; }
+li {text-align: right;}
+.image{width:80%; max-width: 80%; }
+body {background-color: #EEF0F7}
+.container {background-color: #eee; border: thin red dotted; margin: 0 auto; width: 96%; text-align: center;}
+.css3button {
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 1.1rem;
+	color: #ffffff;
+	padding: 2% 5%;
+	background: -moz-linear-gradient(
+		top,
+		#53fc53 0%,
+		#007311);
+	background: -webkit-gradient(
+		linear, left top, left bottom, 
+		from(#E58CFC), to(#9711b8));
+	-moz-border-radius: 10px;
+	-webkit-border-radius: 10px;
+	border-radius: 10px;
+	border: 1px solid #4A025C;
+	-moz-box-shadow:
+		0px 1px 3px rgba(000,000,000,0.5),
+		inset 0px 0px 1px rgba(255,255,255,0.6);
+	-webkit-box-shadow:
+		0px 1px 3px rgba(000,000,000,0.5),
+		inset 0px 0px 1px rgba(255,255,255,0.6);
+	box-shadow:
+		0px 1px 3px rgba(000,000,000,0.5),
+		inset 0px 0px 1px rgba(255,255,255,0.6);
+	text-shadow:
+		0px -1px 0px rgba(0,0,0,1),
+		0px 1px 0px rgba(255,255,255,0.2);
+}
+.css4button {
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 1.1rem;
+	color: #ffffff;
+	padding: 2% 5%;
+	background: -moz-linear-gradient(
+		top,
+		#53fc53 0%,
+		#007311);
+	background: -webkit-gradient(
+		linear, left top, left bottom, 
+		from(#E58CFC), to(#9711b8));
+	-moz-border-radius: 10px;
+	-webkit-border-radius: 10px;
+	border-radius: 10px;
+	border: 1px solid #4A025C;
+	-moz-box-shadow:
+		0px 1px 3px rgba(000,000,000,0.5),
+		inset 0px 0px 1px rgba(255,255,255,0.6);
+	-webkit-box-shadow:
+		0px 1px 3px rgba(000,000,000,0.5),
+		inset 0px 0px 1px rgba(255,255,255,0.6);
+	box-shadow:
+		0px 1px 3px rgba(000,000,000,0.5),
+		inset 0px 0px 1px rgba(255,255,255,0.6);
+	text-shadow:
+		0px -1px 0px rgba(0,0,0,1),
+		0px 1px 0px rgba(255,255,255,0.2);
+}
+</style>
+</head>
+
+<body>
+<div class="container">
+<h1>קריירה בסטיילינג מתחילה פה</h1>
+<img src="http://commondatastorage.googleapis.com/lnd/mob/hat.jpg" class="image"> 
+<h2>בלעדי לסטייליסטים בוגרי קונספט-
+תעודה של אגודת הסטייליסטים בישראל</h2><p style="text-align: center">
+<section>
+            <a href="tel:03-771-3015"/>
+            <input type="tel" value="&#9742; אני רוצה להיות סטייליסטית " class="css4button" /> 
+            </a>
+            </section>
+
+<p style="text-align:right">
+<ul>
+<li>אהבה עמוקה לסטיילינג ואופנה</li>
+<li>תשוקה לקניות, טרנדים, תסרוקות וכל מה שהולך...</li>
+<li>הבטחה לא לסרב להצעות העבודה שאקבל במהלך הלימודים או בסיומם</li>
+<li>לא לקבל " רגליים קרות" כשאצטרך לעשות הפקת אופנה בעצמי </li>
+<li>חברותיות ויכולת עבודה בקבוצה לצד עבודה אישית</li>
+<li>פתיחות לקבלת רעיונות חדשים</li>
+<li>מרפקים להראות מי את באמת!</li>
+</ul>
+</p>
+<a href="go.php" class="css3button" align="center">אני רוצה להיות סטייליסטית</a>
+<h3>תנאים להצלחה כסטייליסטית:</h3>
+</div></body>
