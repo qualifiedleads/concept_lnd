0be3bdf806e146aba087c38cd2bc7e98918c706c
Jen Brannstrom <jen.brannstrom@morequalifiedleads.co.uk>
2014-03-23 13:24:16 +0200

    removes spaces

--- mob/tel.php
+++ mob/tel.php
@@ -6,15 +6,12 @@ header("Location: stylist-cig.php");
 die();
 ?>
 <!DOCTYPE html>
-
 <html lang="en">
 <head>
 <meta http-equiv="content-type" content="text/html; charset=utf-8">
 </head>
 <body>
-
 <img height="1" width="1" border="0" style="display: none;" src="http://trk.conce.pt/tracking202/static/gpx.php?amount=&cid=2" />
-
 </body>
 </html>
 
