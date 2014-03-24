763fe6c9bb8a2c7c40c1c659166876fb554a6e9f
Shailesh Goel <shailesh912@gmail.com>
2014-03-24 15:08:11 +0530

    Update stylist-cig.php

--- mob/stylist-cig.php
+++ mob/stylist-cig.php
@@ -13,15 +13,15 @@
 <title>קריירה בסטיילינג מתחילה פה</title>
 <style>h1{font-size:1.5rem;font-family:Arial,Helvetica,sans-serif}h2{font-size:1.2rem;font-family:Arial,Helvetica,sans-serif}html{font-size:12pt;font-family:Arial,Helvetica,sans-serif}li{text-align:right}.image{width:80%;max-width:80%}body{background-color:#EEF0F7}.container{background-color:#eee;border:thin red dotted;margin:0 auto;width:96%;text-align:center}.css3button,.css4button{font-family:Arial,Helvetica,sans-serif;font-size:1.1rem;color:#fff;padding:2% 5%;background:-moz-linear-gradient(top,#53fc53 0,#007311);background:-webkit-gradient(linear,left top,left bottom,from(#efb73d),to(#a5750d));-moz-border-radius:10px;-webkit-border-radius:10px;border-radius:10px;border:1px solid #00b80c;-moz-box-shadow:0 1px 3px rgba(000,000,000,.5),inset 0 0 1px rgba(255,255,255,.6);-webkit-box-shadow:0 1px 3px rgba(000,000,000,.5),inset 0 0 1px rgba(255,255,255,.6);box-shadow:0 1px 3px rgba(000,000,000,.5),inset 0 0 1px rgba(255,255,255,.6);text-shadow:0 -1px 0 rgba(0,0,0,1),0 1px 0 rgba(255,255,255,.2)}</style>
 </head>
-<body <?php if(isset($_SESSION['callclick'])){ ?> onload="window.location.href='tel:03-771-3015';"<?php } ?>>
+<body>
 <div class="container">
   <h1>קריירה בסטיילינג מתחילה פה</h1>
   <img src="http://commondatastorage.googleapis.com/lnd/mob/cig.jpg" class="image">
   <h2>בלעדי לסטייליסטים בוגרי קונספט-
     תעודה של אגודת הסטייליסטים בישראל</h2>
   <p style="text-align: center">
-  <section> <a <?php if(!isset($_SESSION['callclick'])){?> href="<?php echo $clickThruLink512; ?>" <?php }else{?>href="tel:03-771-3015"<?php unset($_SESSION['callclick']);session_destroy(); }?> id="calllink"/>
-    <input type="tel" value="&#9742; אני רוצה להיות סטייליסטית " class="css4button" <?php if(!isset($_SESSION['callclick'])){?> onclick="return false;" <?php }?> />
+  <section> <a href="<?php echo $clickThruLink512; ?>" id="calllink"/>
+    <input type="tel" value="&#9742; אני רוצה להיות סטייליסטית " class="css4button" />
   </a> </section>
   <p style="text-align:right">
   <ul>
