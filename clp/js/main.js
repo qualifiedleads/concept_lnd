940bb0a676c825d9d0119ea3c3ddebe1ff360f7a
Shailesh <shailesh912@gmail.com>
2015-01-03 02:12:51 +0530

    Concept Landing Page
    Concept Landing Page (Mobile +Desktop) + V.I.Plus Integration + Google
    Conversion Code

---
+++ clp/js/main.js
@@ -0,0 +1,70 @@
+$(document).ready(function() { 
+    
+    $("#contact-form").submit(function(e) {
+        e.preventDefault();
+        $('#error_display').empty();
+        $('.form-field span').empty();
+        $('.form-field').css("margin-bottom","20px");
+        if(validateData()){
+            $.post("subscribe.php", {
+                name: $("#contact-name").val(),
+                email:$("#contact-email").val(),
+                phone: $("#contact-phone").val()
+            }).done(function( data ) {
+                if(data == 1){
+                    var google_conversion_id = 999761620;
+                    var google_conversion_language = "en";
+                    var google_conversion_format = "3";
+                    var google_conversion_color = "ffffff";
+                    var google_conversion_label = "L50SCIfwxVgQ1M3c3AM";
+                    var google_remarketing_only = false;
+                    $.getScript( "http://www.googleadservices.com/pagead/conversion.js" );
+                    $("#error_display").append("Registered Successfully!");
+                }else{
+                    $("#error_display").append(data);
+                }
+            });
+        }
+    }); 
+
+    jQuery('.tfooter .btn').click(function(e){
+        jQuery('html, body').animate({scrollTop:0}, 750, 'linear');
+        e.preventDefault();
+        return false;
+    });  
+    
+});
+
+function validateData(){
+    
+    var fc=0;
+    var fe=0;
+    var fp=0;
+    
+    if($("#contact-name").val()==""){
+        $("#name-error").append("Please enter your Full Name");
+        $("#name-error").parent().css("margin-bottom","10px");
+        fc=1;
+    }
+
+    var emailReg = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+    if(!emailReg.test($("#contact-email").val())) {
+        $("#email-error").append("Please enter a valid Email Address");
+        $("#email-error").parent().css("margin-bottom","10px");
+        fe=1;
+    }
+
+    var phonefilter = /^((\+[1-9]{1,4}[ \-]*)|(\([0-9]{2,3}\)[ \-]*)|([0-9]{2,4})[ \-]*)*?[0-9]{3,4}?[ \-]*[0-9]{3,4}?$/;
+    if (!(phonefilter.test($("#contact-phone").val()) && $.isNumeric($("#contact-phone").val()))) {
+        $("#phone-error").append("Please enter a valid phone number");
+        $("#phone-error").parent().css("margin-bottom","10px");
+        fp=1;
+    }
+
+    if(!(fc && fe && fp)){
+        return true;
+    }else{
+        return false;
+    }
+
+}
---
+++ clp/subscribe.php
@@ -0,0 +1,108 @@
+<?php 
+
+class subscribe{
+
+	public static function saveData($fname, $email, $phone,$lname=""){
+		
+		$fields = array( 
+			'viplists'=>urlencode('79767'),
+			'email' => $email,
+			'firstname' => $fname,
+			'lastname' => $lname,
+			'cellphone' => $phone
+		);
+		
+		$rVal = self::register_viplus('6c575165-e64f-4f80-81e6-749b42c8b4e4', $fields);
+		
+		if($rVal['status'] == 'ok'){
+			return 1;
+		}else{
+			$message = $rVal['error'];
+			return $message;
+		}
+		
+	}
+
+
+	public static function register_viplus($api_key, $param) {
+
+		$port = 80; $timeout = 15;
+		$data = is_array($param) ? http_build_query($param) : $param;
+		$url = parse_url('http://api.viplus.com/Gates/Ws-'.$api_key.'.asmx/RMembers_Import');
+		if ($url['scheme'] != 'http') { die('Error: Only HTTP requests are supported !'); }
+		$host = $url['host'];
+		$path = $url['path'];
+		try{
+			$fp = fsockopen($host, $port, $errno, $errstr, $timeout);
+			if ($fp){
+				fputs($fp, "POST $path HTTP/1.1\r\n");
+				fputs($fp, "Host: $host\r\n");
+				fputs($fp, "Content-type: application/x-www-form-urlencoded\r\n");
+				fputs($fp, "Content-length: ". strlen($data) ."\r\n");
+				fputs($fp, "Connection: close\r\n\r\n");
+				fputs($fp, $data);
+				$result = '';
+				while(!feof($fp))
+					$result .= fgets($fp, 1024);
+			}
+			else return array(
+				'status' => 'error',
+				'error' => "$errstr ($errno)"
+			);
+			fclose($fp);
+			
+			
+			$result = explode("\r\n\r\n", $result, 2);
+			//$header = isset($result[0]) ? $result[0] : '';
+			$content = isset($result[1]) ? $result[1] : '';
+			$rVal = simplexml_load_string($content);
+			$rText = $rVal[0];
+	
+			switch($rText){
+				case 'OkInserted': 
+					return array(
+						'status' => 'ok',
+						'error' => 'Success'
+					);
+					break;
+				case 'ErrNotValid':
+					return array(
+						'status' => 'error',
+						'error' => 'Data contains illegal chars'
+					);
+					break;
+				case 'ErrExists':
+					return array(
+						'status' => 'error',
+						'error' => 'Contact already exists'
+					);
+					break;
+				case 'ErrBlackListed':
+					return array(
+						'status' => 'error',
+						'error' => 'Contact in black list'
+					);
+					break;
+				default:
+					return array(
+						'status' => 'error',
+						'error' => 'Unknown error'
+					);
+					break;
+			}
+		}catch(Exception $e){
+			return array(
+				'status' => 'error',
+				'error' => $e->getMessage()
+			);
+		}
+
+	}
+
+}
+
+$sub = new subscribe();
+
+echo $sub->saveData($_POST['name'], $_POST['email'], $_POST['phone']);
+
+?>
