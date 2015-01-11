1da166bdccb4546d9f62e25632a88b7d0ed97901
Jen Brannstrom <jen.brannstrom@morequalifiedleads.co.uk>
2015-01-11 23:37:34 +0200

    text edits

--- clp/index.php
+++ clp/index.php
@@ -31,35 +31,36 @@ $detect = new Mobile_Detect;
                 </div>
                 <div class="header-row">
                     <div class="header-content">
-                        <h1>_______המסלול המרוכז והמעשי ביותר לקריירה ב</h1>
-                        <p>קונספט היא פלטפורמה למעצבים יזמים
-המסלול מתאים למי שבאמת רוצה להצליח כמעצב ורואה עצמו מפתח קריירה מקצועית בתחום </p>
+                        <h1>לימודי עיצוב פנים עם השמה מקצועית</h1>
+                        <p>מכללת קונספט מתחייבת להגשים לכם את החלום ולהפוך אתכם למעצבים מצליחים.
+בכיתות קטנות ויחס אישי, עם מרצים מהשורה הראשונה בארץ, תקבלו את כל הידע הדרוש 
+במרוכז וכולל התמחות מעשית במהלך הלימודים!</p>
                         <div class="header-video">
                             <iframe src="https://player.vimeo.com/video/115902396?portrait=0&amp;wmode=opaque" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen="" id="videohead"></iframe>
                         </div>
                     </div>
                     <div class="header-form">
-                        <h1>לקבלת תאריכי פתיחה ותכנית לימוד מלאה</h1>
+                        <h1>לקבלת תאריכי פתיחה ותכנית לימוד מלאה, הרשמו כאן:</h1>
                         <div id="error_display"></div>
                         <form method="post" id="contact-form" autocomplete="off">
                             <div class="form-field">
-                                <label for="contact-name">Full Name *</label>
+                                <label for="contact-name">שם מלא *</label>
                                 <input type="text" name="name" id="contact-name" class="text">
                                 <span id="name-error"></span>
                             </div>
                             <div class="form-field">
-                                <label for="contact-email">Email *</label>
+                                <label for="contact-email">אימייל *</label>
                                 <input type="text" name="email" id="contact-email" class="text">
                                 <span id="email-error"></span>
                             </div>
                             <div class="form-field">
-                                <label for="contact-phone">Phone Number *</label>
+                                <label for="contact-phone">טלפון סלולארי*</label>
                                 <input type="text" name="phone" id="contact-phone" class="text">
                                 <span id="phone-error"></span>
                             </div>
                             <div class="form-btn">
                                 <button type="submit" class="btn button" name="contact-form-btn">
-                                  <p>Submit Your Information</p>
+                                  <p>שלחו לי עכשיו</p>
                                   <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" enable-background="new 0 0 512 512" xml:space="preserve">
                                     <path id="paper-plane-icon" d="M462,54.955L355.371,437.187l-135.92-128.842L353.388,167l-179.53,124.074L50,260.973L462,54.955z
                                 M202.992,332.528v124.517l58.738-67.927L202.992,332.528z"></path> 
@@ -67,7 +68,7 @@ $detect = new Mobile_Detect;
                                 </button>
                                 <!--<input type="submit" value="Submit Your Information" class="btn button" name="contact-form-btn">-->
                             </div>
-							<p>מפגש התרשמות מרוכז פעם בחודש. הזמנה תשלח במייל</p>
+							<p>מתנה מיוחדת בשווי 600 ש' לפונים מדף זה</p>
                         </form>
                     </div>
                </div>
@@ -78,18 +79,20 @@ $detect = new Mobile_Detect;
                 <div class="fitem-row">
                     <div class="fitem">
                         <a href="#" style="background-image:url(img/icons/guarantee.png)"></a>
-                        <h2>המסלול המותאם לצורכי התעשייה</h2>
-                        <p>לימודים ממוקדים הכוללים סטז'ים כבר במהלך הלימודים- מסיימים עם ניסיון!</p>
+                        <h2>התמחות מעשית במהלך הלימודים</h2>
+                        <p>לימודים ממוקדים הכוללים סטזי'ם כבר במהלך הלימודים.
+מסיימים עם נסיון!</p>
                     </div>
                     <div class="fitem">
                         <a href="#" style="background-image:url(img/icons/lamp.png)"></a>
-                        <h2>להיות מעצב עצמאי</h2>
-                        <p>ערכת יזמות עסקית למעצבים עצמאיים( כולל בנייה וניהול מותג אישי)</p>
+                        <h2>יזמות עיסקית למעצבים</h2>
+                        <p>תוכנית ייחודית למעצבים יזמים 
+אשר מעוניינם להקים ולנהל מותג אישי</p>
                     </div>
                     <div class="fitem">
                         <a href="#" style="background-image:url(img/icons/dollar.png)"></a>
                         <h2>עבודה בעיצוב</h2>
-                        <p>השמה תעסוקתית לבוגרי בקונספט להשתלבות מהירה כמעצבים</p>
+                        <p>השמה תעסוקתית לבוגרי קונספט להשתלבות מהירה כמעצבים</p>
                     </div>
                     <div class="fitem">
                         <a href="#" style="background-image:url(img/icons/mortar.png)"></a>
@@ -101,42 +104,42 @@ $detect = new Mobile_Detect;
         </div>
         <div class="products">
             <div class="container">
-                <div class="pitem-heading">The Beauty of Contrast</div>
-                <div class="pitem-subheading">We turn you on to pro lighting strategies to highlight the most of indoor and outdoor rooms</div>
+                <div class="pitem-heading">הסטודנטים שלנו מצליחים!</div>
+                <div class="pitem-subheading">93% מבוגרי קונספט משתלבים בתחום העיצוב מיד עם תום הלימודים</div>
                  <?php if(!($detect->isMobile() || $detect->isTablet())):?>
                 <div class="pitem-row">
                     <div class="pitem">
                         <img src="img/1.jpg">
-                        <h2>Beautiful New Kitchen</h2>
-                        <p>great deal with awesome new features it is realy incredible design and price.</p>
+                        <h2>עיצוב חדרי רחצה במלון, אילת 2014</h2>
+                        <p>לילך הוד, בוגרת קונספט המכללה לעיצוב</p>
                     </div>
                     <div class="pitem">
                         <img src="img/2.jpg">
-                        <h2>Beautiful New Kitchen</h2>
-                        <p>great deal with awesome new features it is realy incredible design and price.</p>
+                        <h2>עיצוב חדרי שינה, לונדון 2014</h2>
+                        <p>ניר מלך, בוגר קונספט המכללה לעיצוב</p>
                     </div>
                     <div class="pitem">
                         <img src="img/3.jpg">
-                        <h2>Beautiful New Kitchen</h2>
-                        <p>great deal with awesome new features it is realy incredible design and price.</p>
+                        <h2>עיצוב לופט, תל אביב 2012</h2>
+                        <p>רעות גושן, בוגרת קונספט המכללה לעיצוב</p>
                     </div>
                 </div>
                 <?php endif;?>
                 <div class="pitem-row">
                     <div class="pitem">
                         <img src="img/4.jpg">
-                        <h2>Beautiful New Kitchen</h2>
-                        <p>great deal with awesome new features it is realy incredible design and price.</p>
+                        <h2>עיצוב חדר לתינוק, חיפה 2013</h2>
+                        <p>גלית שם טוב, בוגרת קונספט המכללה לעיצוב</p>
                     </div>
                     <div class="pitem">
                         <img src="img/5.jpg">
-                        <h2>Beautiful New Kitchen</h2>
-                        <p>great deal with awesome new features it is realy incredible design and price.</p>
+                        <h2>עיצוב מטבח וחדר אוכל, כפר ויתקין 2014</h2>
+                        <p>מירי מסיקה, בוגרת קונספט המכללה לעיצוב</p>
                     </div>
                     <div class="pitem">
                         <img src="img/6.jpg">
-                        <h2>Beautiful New Kitchen</h2>
-                        <p>great deal with awesome new features it is realy incredible design and price.</p>
+                        <h2>עיצוב חללים מסחריים (מספרה), רמת השרון 2015</h2>
+                        <p>רונן גבע, בוגר קונספט המכללה לעיצוב</p>
                         <!--<div class="pitem-btns">
                             <div class="btn-one">40 m<sup>2</sup></div>
                             <div class="btn-two">Total Price: $15,000</div>
@@ -147,19 +150,20 @@ $detect = new Mobile_Detect;
         </div>
         <div class="testimonials">
             <div class="container">
-                <div class="testimonials-heading">בוגרים טריים</div>
-                <div class="testimonials-subheading">Sub text</div>
+                <div class="testimonials-heading">בוגרי המסלול לעיצוב פנים ממליצים:</div>
+
                 <div class="testimonials-row">
                     <div class="testimonial">
                         <div class="tinr">
                             <div class="timg" style="background-image:url(img/meirav.jpg)"></div>
-                            <p>הלימודים בקונספט הם ההשקעה הכי טובה שעשיתי בחיים שלי, כבר במהלך הלימודים התחלתי לעבוד בתחום, כך שההסבה המקצועית שלי" קרתה הרבה יותר מהר ממה שדמיינתי..."<br><span style="color:#66cc33;">&nbsp;</span><span style="color:#d3d3d3;">בוגר</span><span style="color:#66cc33;"> </span><span style="color:#0099ff;">מירב גרוסמן</span></p>
+                            <p>הלימודים בקונספט הם ההשקעה הכי טובה שעשיתי בחיים שלי, כבר במהלך הלימודים התחלתי לעבוד בתחום, כך שההסבה המקצועית שתכננתי קרתה הרבה יותר מהר ממה שדמיינתי.
+בזכות התוכנית ליזמים של קונספט, הצלחתי לקבל עבודות ופרוייקטים גדולים<span style="color:#66cc33;">&nbsp;</span><span style="color:#66cc33;"> </span><br><span style="color:#0099ff;">מירב גרוסמן</span></p>
                         </div>
                     </div>
                     <div class="testimonial">
                         <div class="tinr">
                             <div class="timg" style="background-image:url(img/shirli.jpg)"></div>
-                            <p>מתוך אהבה גדולה לעיצוב פנים נרשמתי ללימודים בקונספט, שמכל הסילבוסים היו הכי מעשיים וממוקדים בעיני. במסלול עצמו גיליתי" עד כמה תכני הלימודים פרקטיים ומכינים אותנו למציאות, כך שבקלות התקבלתי לעבודה כמעצבת פנים."<br><span style="color:#66cc33;">&nbsp;</span><span style="color:#d3d3d3;">בוגר</span><span style="color:#66cc33;">&nbsp;שירלי חיון</span></p>
+                            <p>מתוך אהבה גדולה לעיצוב פנים נרשמתי ללימודים בקונספט, שמכל הסילבוסים היו הכי מעשיים וממוקדים בעיני. במסלול עצמו גיליתי" עד כמה תכני הלימודים פרקטיים ומכינים אותנו למציאות, כך שבקלות התקבלתי לעבודה כמעצבת פנים."<br><span style="color:#66cc33;">&nbsp;</span><span style="color:#66cc33;">&nbsp;שירלי חיון</span></p>
                         </div>
                     </div>
                 </div>
@@ -199,21 +203,19 @@ $detect = new Mobile_Detect;
         </div>
         <div class="tfooter">
             <div class="container">
-                <h1>What are you waiting for?</h1>
-                <p>We turn you on to pro lighting strategies to highlight features create intrigue and make the most of indoor and outdoor rooms</p>
-                <div class="btn-class"><button class="btn">Submit Your Information</button></div>
+                <h1>אז למה את מחכה?</h1>
+                <p>אם חשוב לך ללמוד במקום מוכר ע"י משרד המסחר והתעסוקה, משרד הקליטה, ביטוח לאומי, משרד הביטחון ועוד,
+אם חשוב לך ללמוד במקום שמתחייב להצלחה שלך, צרי איתנו קשר כעת לתאום פגישת ייעוץ והכוונה אישית, ללא תשלום.
+</p>
+                <div class="btn-class"><button class="btn">גם אני רוצה להצליח בעיצוב!</button></div>
             </div>
         </div>
         <div class="lfooter">
             <div class="container">
-                <div class="social"> 
-                    <div class="twitter_container"></div>
-                    <div class="google_container"></div>
-                    <div class="facebook_container"></div>
-                </div>
+
                 <div class="copyright">
                    <p><span>Made with</span>
-                    <img src="http://d9hhrg4mnvzow.cloudfront.net/unbouncepages.com/master-desktop/1kvyvol-heart-small.png" alt=""><span>in Earth | All rights reserved Copyright © 2014 Concept</span><p>
+                    <img src="http://d9hhrg4mnvzow.cloudfront.net/unbouncepages.com/master-desktop/1kvyvol-heart-small.png" alt=""><span>in Tel Aviv | All rights reserved Copyright © 2014 Concept Academy</span><p>
                 </div>
             </div>
         </div>
